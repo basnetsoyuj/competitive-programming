f1fea5501c7a883aafe29471d9901cfce69e2d3d
Soyuj Jung Basnet <bsoyuj@gmail.com>
2022-05-09 17:08:36 +0400

    Majority Element question from leetcode

---
+++ leetcode/169. Majority Element/README.md
@@ -0,0 +1,40 @@
+## 169. Majority Element
+
+Given an array `nums` of size `n`, return the <i>majority</i> element.
+
+The majority element is the element that appears more than `⌊n / 2⌋` times. You may assume that the majority element always exists in the array.
+
+### Examples
+
+**Input**
+```
+nums = [3,2,3]
+```
+
+**Output**
+```
+3
+```
+
+---
+
+**Input**
+```
+nums = [2,2,1,1,1,2,2]
+```
+
+**Output**
+```
+2
+```
+
+---
+
+**Constraints**
+* `n == nums.length`
+* <code>1 <= n <= 5 * 10<sup>4</sup></code>
+* <code>-10<sup>9</sup> <= nums[i] <= 10<sup>9</sup></code>
+
+---
+
+**Follow-up:** Could you solve the problem in linear time and in `O(1)` space.
---
+++ leetcode/169. Majority Element/main.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+class Solution
+{
+public:
+    int majorityElement(std::vector<int> &nums)
+    {
+        // set the first element as the majority element initially
+        int majority = nums[0];
+
+        // has frequency 1
+        int freq(1);
+
+        for (int i = 1; i < nums.size(); i++)
+        {
+            // increment freq if same majority is encounterd
+            if (nums[i] == majority)
+                freq++;
+            // otherwise decrement freq
+            else
+                freq--;
+            // switch majority if freq goes below 0
+            if (freq < 0)
+            {
+                majority = nums[i];
+                freq = 1;
+            }
+        }
+        // it is bound to work as if majority is cancelled out by other elements
+        // it will be reintroduced as frequency is reset
+        return majority;
+    }
+};
+
+int main()
+{
+    // input array
+    // int numbers[] = {3, 2, 3};
+    int numbers[] = {2, 3, 2, 1, 3, 1, 3, 1, 2, 2};
+
+    std::vector<int> nums(numbers, numbers + sizeof(numbers) / sizeof(int));
+
+    Solution solver;
+    std::cout << solver.majorityElement(nums);
+
+    return 0;
+}
