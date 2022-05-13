33b9b34a64bb2b52d308dfccb79d38fe143c3111
Soyuj Jung Basnet <bsoyuj@gmail.com>
2022-05-13 22:40:34 +0400

    Leetcode 9

---
+++ leetcode/9. Palindrome Number/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        string str = to_string(x);
+        int i = str.length() / 2;
+
+        if (str.length() % 2)
+            i--;
+
+        for (; i >= 0; i--)
+            if (str[i] != str[str.length() - 1 - i])
+                return false;
+
+        return true;
+    }
+};
+
+int main()
+{
+    Solution solver;
+    cout << solver.isPalindrome(121) << endl;
+
+    return 0;
+}
