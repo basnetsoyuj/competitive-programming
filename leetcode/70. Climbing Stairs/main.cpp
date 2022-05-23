d41edfd882fce4f5dca5983971ce43dcd36c496e
Soyuj Jung Basnet <bsoyuj@gmail.com>
2022-05-23 14:25:48 +0400

    leetcode 70

---
+++ leetcode/70. Climbing Stairs/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        int M[46];
+        M[0] = 1;
+        M[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            M[i] = M[i - 1] + M[i - 2];
+        }
+
+        return M[n];
+    }
+};
