8a103793fbf22c7b3078a29364b5422ac15025fd
Soyuj Jung Basnet <bsoyuj@gmail.com>
2022-06-03 00:10:38 -0500

    leetcode 66

---
+++ leetcode/66. Plus One/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+        int i = digits.size() - 1;
+
+        while (i >= 0 && digits[i] == 9)
+        {
+            digits[i] = 0;
+            i--;
+        }
+
+        if (i < 0)
+            digits.insert(digits.begin(), 1);
+        else
+            digits[i]++;
+
+        return digits;
+    }
+};
