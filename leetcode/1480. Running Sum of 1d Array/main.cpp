0ce744100c4bb74743086aa0429ad56e2ae42ec1
Soyuj Jung Basnet <bsoyuj@gmail.com>
2022-06-05 09:02:58 -0500

    leetcode 1480

---
+++ leetcode/1480. Running Sum of 1d Array/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> runningSum(vector<int> &nums)
+    {
+        int sum = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+            nums[i] = (sum += nums[i]);
+
+        return nums;
+    }
+};
