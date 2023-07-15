766a90bb497d37d427b834eea20c89cba8a3acbd
Soyuj Jung Basnet <bsoyuj@gmail.com>
2023-07-15 18:57:56 -0400

    Leetcode 2778

---
+++ leetcode/2778. Sum of Squares of Special Elements/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int sumOfSquares(vector<int> &nums)
+    {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++)
+            if (nums.size() % (i + 1) == 0)
+                ans += pow(nums[i], 2);
+        return ans;
+    }
+};
